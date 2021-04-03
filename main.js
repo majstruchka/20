acfe4273026c300e17930c37b82f98b4060daf8f
Majstruchka <maysstruk@ukr.net>
2021-04-03 08:34:18 +0300

    3

--- main.js
+++ main.js
@@ -8,19 +8,28 @@ console.log('Результат :');
 console.log('--------------------');
 
 result = a + b;
-console.log('a1 + b =  ', result);
+console.log('a + b =  ', result);
 
 
 result = a - b;
-console.log('a1 - b = ', result);
+console.log('a - b = ', result);
 
 result = a * b;
-console.log('a1 * b = ', result);
+console.log('a * b = ', result);
 
 
 result = a / b;
 
-console.log('a1 / b = ', result);
+console.log('a / b = ', result);
 
 console.log('--------------------');
 console.log('**************');
+
+document.write("<p>При а = 155, b = 9</p>");
+
+document.write("Результат : ");
+
+
+document.write("a + b = 164; a - b = 146; a * b = 1395; a / b = 17,2222;");
+
+
